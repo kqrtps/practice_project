0bcd98197df340f807b254b690f1f1d324543e0f
annyakost <kost.anna1@student.uzhnu.edu.ua>
2025-06-20 19:38:53 +0300

    Оновлено schemas.py: заміна orm_mode на from_attributes для Pydantic v2

--- schemas.py
+++ schemas.py
@@ -10,4 +10,4 @@ class LocationRead(LocationBase):
     location_id: int
 
     class Config:
-        orm_mode = True
+        from_attributes = True
---
+++ views.py
@@ -0,0 +1,24 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+import crud
+import schemas
+from database import SessionLocal
+
+router = APIRouter()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@router.get("/locations/{location_id}", response_model=schemas.LocationRead)
+def read_location(location_id: int, db: Session = Depends(get_db)):
+    db_location = crud.get_location(db, location_id)
+    if db_location is None:
+        raise HTTPException(status_code=404, detail="Location not found")
+    return db_location
+
+
+
