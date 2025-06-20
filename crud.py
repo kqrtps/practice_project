f79bac6e4f636218ebe7b4fd1ba0c704f32b4e45
annyakost <kost.anna1@student.uzhnu.edu.ua>
2025-06-20 14:49:30 +0300

    Додано CRUD функції для locations

--- crud.py
+++ crud.py
@@ -1,3 +1,13 @@
 from sqlalchemy.orm import Session
 from models import Location
+from schemas import LocationCreate
 
+def get_location(db: Session, location_id: int) -> Location | None:
+    return db.query(Location).filter(Location.location_id == location_id).first()
+
+def delete_location(db: Session, location_id: int) -> Location | None:
+    db_location = db.query(Location).filter(Location.location_id == location_id).first()
+    if db_location:
+        db.delete(db_location)
+        db.commit()
+    return db_location
