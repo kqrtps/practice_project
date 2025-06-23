0fb9821578b5d756fae03b52967a1a767687e983
kqrtps <a66375338@gmail.com>
2025-06-23 16:26:34 +0300

    added user functions and endpoints

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
-from views import router  # імпортуємо router з views.py
+from views import router , router_user # імпортуємо router з views.py
 
 app = FastAPI()
 
 app.include_router(router)
+app.include_router(router_user)
--- schemas.py
+++ schemas.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel
+from typing import Optional
 
 class LocationBase(BaseModel):
     location_name: str
@@ -14,3 +15,25 @@ class LocationRead(LocationBase):
 
 class LocationUpdate(LocationBase):
     pass
+
+
+
+class UserBase(BaseModel):
+    username:str
+
+class UserRead(UserBase):
+    user_id:int
+    location_id:int
+
+    class Config:
+        from_attributes = True
+
+class UserCreate(UserBase):
+    password: str
+    location_id: int
+
+class UserUpdate(BaseModel):
+    username: Optional[str] = None
+    password: Optional[str] = None
+    location_id: Optional[int] = None
+
